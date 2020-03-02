4ca753f1ed9ba2a4953413399edffe5150fecc5c
nadinho <rockt2935@gmail.com>
2020-03-02 14:51:20 +0100

    Add webpack config and scripts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = 'Hallo';
-
-  return element;
-}
-const div = component();
-document.body.appendChild(div);
+import { pokemon } from './components/pokemon';
+document.body.appendChild(pokemon());
