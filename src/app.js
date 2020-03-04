8b8a137715c2727d2ac34c44063e694961f1da62
nadinho <rockt2935@gmail.com>
2020-03-04 16:10:02 +0100

    rename pokemons and outsource function

--- src/app.js
+++ src/app.js
@@ -4,170 +4,8 @@ import { createTitle } from './components/title';
 import { createSearch } from './components/search';
 import { createPokemons } from './components/pokemons';
 import { appendContent } from './lib/dom';
+import { filterPokemons } from './lib/dom';
 import Logo from './assets/fav_pichu.png';
-
-const allPokemons = [
-  'Bulbasaur',
-  'Ivysaur',
-  'Venusaur',
-  'Charmander',
-  'Charmeleon',
-  'Charizard',
-  'Squirtle',
-  'Wartortle',
-  'Blastoise',
-  'Caterpie',
-  'Metapod',
-  'Butterfree',
-  'Weedle',
-  'Kakuna',
-  'Beedrill',
-  'Pidgey',
-  'Pidgeotto',
-  'Pidgeot',
-  'Rattata',
-  'Raticate',
-  'Spearow',
-  'Fearow',
-  'Ekans',
-  'Arbok',
-  'Pikachu',
-  'Raichu',
-  'Sandshrew',
-  'Sandslash',
-  'Nidoran',
-  'Nidorina',
-  'Nidoqueen',
-  'Nidoran',
-  'Nidorino',
-  'Nidoking',
-  'Clefairy',
-  'Clefable',
-  'Vulpix',
-  'Ninetales',
-  'Jigglypuff',
-  'Wigglytuff',
-  'Zubat',
-  'Golbat',
-  'Oddish',
-  'Gloom',
-  'Vileplume',
-  'Paras',
-  'Parasect',
-  'Venonat',
-  'Venomoth',
-  'Diglett',
-  'Dugtrio',
-  'Meowth',
-  'Persian',
-  'Psyduck',
-  'Golduck',
-  'Mankey',
-  'Primeape',
-  'Growlithe',
-  'Arcanine',
-  'Poliwag',
-  'Poliwhirl',
-  'Poliwrath',
-  'Abra',
-  'Kadabra',
-  'Alakazam',
-  'Machop',
-  'Machoke',
-  'Machamp',
-  'Bellsprout',
-  'Weepinbell',
-  'Victreebel',
-  'Tentacool',
-  'Tentacruel',
-  'Geodude',
-  'Graveler',
-  'Golem',
-  'Ponyta',
-  'Rapidash',
-  'Slowpoke',
-  'Slowbro',
-  'Magnemite',
-  'Magneton',
-  "Farfetch'd",
-  'Doduo',
-  'Dodrio',
-  'Seel',
-  'Dewgong',
-  'Grimer',
-  'Muk',
-  'Shellder',
-  'Cloyster',
-  'Gastly',
-  'Haunter',
-  'Gengar',
-  'Onix',
-  'Drowzee',
-  'Hypno',
-  'Krabby',
-  'Kingler',
-  'Voltorb',
-  'Electrode',
-  'Exeggcute',
-  'Exeggutor',
-  'Cubone',
-  'Marowak',
-  'Hitmonlee',
-  'Hitmonchan',
-  'Lickitung',
-  'Koffing',
-  'Weezing',
-  'Rhyhorn',
-  'Rhydon',
-  'Chansey',
-  'Tangela',
-  'Kangaskhan',
-  'Horsea',
-  'Seadra',
-  'Goldeen',
-  'Seaking',
-  'Staryu',
-  'Starmie',
-  'Mr. Mime',
-  'Scyther',
-  'Jynx',
-  'Electabuzz',
-  'Magmar',
-  'Pinsir',
-  'Tauros',
-  'Magikarp',
-  'Gyarados',
-  'Lapras',
-  'Ditto',
-  'Eevee',
-  'Vaporeon',
-  'Jolteon',
-  'Flareon',
-  'Porygon',
-  'Omanyte',
-  'Omastar',
-  'Kabuto',
-  'Kabutops',
-  'Aerodactyl',
-  'Snorlax',
-  'Articuno',
-  'Zapdos',
-  'Moltres',
-  'Dratini',
-  'Dragonair',
-  'Dragonite',
-  'Mewtwo',
-  'Mew'
-];
-
-function filterPokemons(searchValue) {
-  const lowerCaseSearchValue = searchValue.toLowerCase();
-
-  const filteredPokemons = allPokemons.filter(pokemon => {
-    return pokemon.toLowerCase().includes(lowerCaseSearchValue);
-  });
-  return filteredPokemons;
-}
 
 export function app() {
   const header = createElement('header', {
@@ -183,19 +21,19 @@ export function app() {
     src: Logo
   });
 
-  let pokemons = null;
+  let searchResults = null;
   function setSearchResults() {
     const filteredPokemons = filterPokemons(searchInput.value);
-    pokemons = createPokemons(filteredPokemons);
-    appendContent(main, pokemons);
+    searchResults = createPokemons(filteredPokemons);
+    appendContent(main, searchResults);
   }
   setSearchResults();
 
   appendContent(header, [title]);
-  appendContent(main, [logo, searchInput, pokemons]);
+  appendContent(main, [logo, searchInput, searchResults]);
 
   searchInput.addEventListener('input', event => {
-    main.removeChild(pokemons);
+    main.removeChild(searchResults);
     setSearchResults();
 
     const searchValue = event.target.value;
