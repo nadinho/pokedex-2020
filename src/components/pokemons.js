b2e2380331892af6c810f09e9832e8725cdbf860
nadinho <rockt2935@gmail.com>
2020-03-05 12:22:02 +0100

    outsource filterPokemon

--- src/components/pokemons.js
+++ src/components/pokemons.js
@@ -1,19 +1,30 @@
 import './pokemons.scss';
 import { createElement, appendContent } from '../lib/dom';
 
-export function createPokemons(items) {
+export function createSearchResults(props) {
   const container = createElement('div', {
     className: 'pokemons'
   });
 
-  items.forEach(item => {
+  props.items.forEach(item => {
     const element = createElement('div', {
       innerText: item,
       className: 'pokemon'
     });
     element.addEventListener('click', () => {
-      const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-      favorites.push(item);
+      let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+      // if (!favorites.includes(item)) {
+      //   favorites.push(item);
+      // } else {
+      //   const itemIndex = favorites.indexOf(item);
+      //   favorites.splice(itemIndex, 1);
+      // }
+
+      // if (favorites.length > 4) {
+      //   favorites.splice(0, 1);
+      // }
+
       const favoritesJSON = JSON.stringify(favorites);
       localStorage.setItem('favorites', favoritesJSON);
     });
